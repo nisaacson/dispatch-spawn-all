48c281fc6d7de5d204d342e025b1cadf29bffc5c
Noah Isaacson <clewfirst@gmail.com>
2013-05-13 13:53:50 -0400

    added should spawn check

---
+++ test/spawn-test.js
@@ -0,0 +1,26 @@
+var should = require('should')
+var rewire = require('rewire')
+describe('Spawn All', function () {
+  it('should spawn all commands', function (done) {
+    var spawnAll = rewire('../')
+    var spawnCount = 0
+    spawnAll.__set__('spawn', function (data, cb) {
+      spawnCount += 1
+      cb()
+    })
+    var commands = [
+      {
+        host: 'localhost',
+        port: 7000,
+        secret: 'foo_secret',
+        command: 'echo "hello world"',
+        repoDir: 'apples'
+      }
+    ]
+    spawnAll(commands, function (err) {
+      should.not.exist(err, 'error spawning commands: ' + JSON.stringify(err, null, ' '))
+      spawnCount.should.eql(commands.length)
+      done()
+    })
+  })
+})
